179df657f2b46b6302610bc1316930e30feb0e24
Paranjay Srivastava <paranjay.pj@gmail.com>
2014-11-22 21:09:53 -0500

    adding Job bean and xhtml

---
+++ src/com/jobeye/JobBean.java
@@ -0,0 +1,58 @@
+package com.jobeye;
+
+import javax.ejb.EJB;
+
+import com.jobeye.EJB.Service.*;
+
+public class JobBean 
+{
+	private long jobId;
+	private int companyId; 
+	private String position;
+	private String location;
+
+	public long getJobId() 
+	{	return jobId;	}
+
+	public void setJobId(long jobId) 
+	{	this.jobId = jobId;	}
+
+	public long getCompanyId() {
+		return companyId;
+	}
+
+	public void setCompanyId(int companyId) {
+		this.companyId = companyId;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	} 
+	
+
+	@EJB
+	//private jobsession userAdd;
+	com.jobeye.EJB.Service.JobSession jobSession;
+	
+	public String AddJob()
+	{
+String ret = jobSession.AddJob(companyId, location, position);
+		if(ret.equalsIgnoreCase("Exists")){
+			return "false";
+		}
+		return "submit";
+	}
+	
+}
