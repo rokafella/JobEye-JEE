4595b84d34ec8240284dcb71b96fbbf3927f6d1f
Rohit Kapoor <rohit6301@gmail.com>
2014-12-08 21:02:29 -0500

    Schema Fix

--- sql/schema.sql
+++ sql/schema.sql
@@ -10,12 +10,11 @@ drop table Application;
 
 create table User(userId long, name varchar(50), email varchar(50) UNIQUE, phone varchar(20), password varchar(50));
 create table Task(taskId integer, date date, description varchar(50), applicationId integer);
---create table Stage(stageId integer, description varchar(50), stageType varchar(30), applicationId integer);
 create table Profile(profileId long, profileType varchar(50), userId long, primary key(profiletype,userid));
 create table Job(jobId integer, companyId integer, position varchar(50), Location varchar(50));
 create table Company(companyId integer, name varchar(50), headQuarters varchar(50), userId integer);
 create table Activity(activityId integer, description varchar(50), title varchar(50), date date, companyId integer);
-create table Application(applicationId integer, jobId integer, profileId integer, status varchar(50) description (300));
+create table Application(applicationId integer, jobId integer, profileId integer, status varchar(50), description varchar(300));
 
 select * from activity;
 select * from application;
